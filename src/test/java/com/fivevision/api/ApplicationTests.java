9b9239f8e5ab3d4167f652fafd443773dab11cf2
Alitarek3102005 <tali19985@gmail.com>
2026-09-02 19:50:08 +0300

    Initial commit

---
+++ src/test/java/com/fivevision/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivevision.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
